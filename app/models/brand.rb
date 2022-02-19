7ba0fcfae3e11243231b90ddb5f49a53e13138b5
Han Fiden <fidendu2@gmail.com>
2022-02-19 17:54:47 +0100

    Add array of tags and categories

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,6 @@
 class Brand < ApplicationRecord
-  has_many :products
+  has_many :products, dependent: :destroy
+
+  TAGS = ['', 'Fabriqué en France 🇫🇷', 'Fait main 🙌', 'Non vendu sur Amazon ❌', 'Produits locaux 📍', 'Upcyclé ♻️', 'Éco-responsable 🌱']
+  CATEGORIES = ['', 'Accessoires', 'Beauté', 'Bijoux', 'Épicerie', 'Déco', 'Mode', 'Papeterie', 'Enfants']
 end
