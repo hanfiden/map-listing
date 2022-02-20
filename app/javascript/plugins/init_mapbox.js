c9a0c1e7330454b8adafc7c477a7a6c2b11d1020
Han Fiden <fidendu2@gmail.com>
2022-02-20 18:07:32 +0100

    Add new feature to mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 10 });
 };
 
 const initMapbox = () => {
@@ -12,18 +12,72 @@ const initMapbox = () => {
 
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
     const map = new mapboxgl.Map({
       container: "map",
-      style: "mapbox://styles/mapbox/streets-v11"
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [2.209666999999996, 46.232192999999995],
+      zoom: 5
     });
+
+    ////////////////// OPTIONS ON THE MAP ////////////////////
+    const nav = new mapboxgl.NavigationControl()
+    const full_screen = new mapboxgl.FullscreenControl({ container: document.querySelector('.body') })
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true
+    })
+
+    ////////////////// EVENTS ON THE MAP ////////////////////
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: true
+      })
+    ).addControl(nav)
+      .addControl(geolocate)
+      .addControl(full_screen);
+
+    ////////////////// PARSE EACH DATA TO THE MAP ////////////////////
     const markers = JSON.parse(mapElement.dataset.markers);
+    // console.log(markers)
+
     markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-      new mapboxgl.Marker()
+      ////////////////// OPTIONS ON THE MARKER ////////////////////
+      const popup = new mapboxgl.Popup({
+        offset: 25,
+        closeOnClick: false
+      })
+        .setHTML(marker.info_window);
+
+      const result_marker = new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
-        .addTo(map);
+        .addTo(map)
+
+      ////////////////// EVENTS ON THE MARKER ////////////////////
+      result_marker.getElement().addEventListener('mouseenter', () => {
+        result_marker.togglePopup();
+      });
+
+      result_marker.getElement().addEventListener('mouseleave', () => {
+        result_marker.togglePopup()
+        // popup.remove(map)
+      });
+
+      result_marker.getElement().addEventListener('click', (e) => {
+        map.flyTo({
+          center: [marker.lng, marker.lat],
+          zoom: 12
+        });
+      });
+
     });
+
     fitMapToMarkers(map, markers);
   }
 };
