586be74ed4dd13740f16e26859b91a2840e4e6c7
Han Fiden <fidendu2@gmail.com>
2022-02-20 19:00:52 +0100

    Add advanced_search method to model

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,18 +1,18 @@
 class Brand < ApplicationRecord
   has_many :products, dependent: :destroy
 
-  TAGS = ['Fabriqué en France 🇫🇷', 'Fait main 🙌', 'Non vendu sur Amazon ❌', 'Produits locaux 📍', 'Upcyclé ♻️', 'Éco-responsable 🌱']
-  CATEGORIES = ['Accessoires', 'Beauté', 'Bijoux', 'Épicerie', 'Déco', 'Mode', 'Papeterie', 'Enfants']
+  TAGS = ['', 'Fabriqué en France 🇫🇷', 'Fait main 🙌', 'Non vendu sur Amazon ❌', 'Produits locaux 📍', 'Upcyclé ♻️', 'Éco-responsable 🌱']
+  CATEGORIES = ['', 'Accessoires', 'Beauté', 'Bijoux', 'Épicerie', 'Déco', 'Mode', 'Papeterie', 'Enfants']
 
   geocoded_by :full_address
   after_validation :geocode
 
-  include PgSearch::Model
-  pg_search_scope :full_search,
-                  against: %i[category tags city_name],
-                  using: {
-                    tsearch: { any_word: true }
-                  }
+  include PgSearch
+  pg_search_scope :advanced_search, (lambda do |args, query|
+    return {
+      against: args, query: query
+    }
+  end)
 
   def full_address
     "#{address_name}, #{zip_code}, #{city_name}"
