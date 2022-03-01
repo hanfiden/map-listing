0cfa6e44c5419aa935dcbbd84b17cbdcb8946343
Han Fiden <fidendu2@gmail.com>
2022-03-01 03:20:30 +0100

    Succesfuly rendered ajax test airports

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,6 +9,9 @@ const fitMapToMarkers = (map, markers) => {
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
+  const listingEl = document.getElementById('feature-listing');
+  const filterEl = document.getElementById('feature-filter');
+  let brands = [];
 
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -20,6 +23,162 @@ const initMapbox = () => {
       zoom: 5,
       doubleClickZoom: false
     });
+
+
+    function renderListings(features) {
+      const empty = document.createElement('p');
+      // Clear any existing listings
+      listingEl.innerHTML = '';
+      if (features.length) {
+        for (const feature of features) {
+          const itemLink = document.createElement('a');
+          const label = `${feature.properties.name} (${feature.properties.abbrev})`;
+          itemLink.href = feature.properties.wikipedia;
+          itemLink.target = '_blank';
+          itemLink.textContent = label;
+          itemLink.addEventListener('mouseover', () => {
+            // Highlight corresponding feature on the map
+            popup
+              .setLngLat(feature.geometry.coordinates)
+              .setText(label)
+              .addTo(map);
+          });
+          listingEl.appendChild(itemLink);
+        }
+
+        // Show the filter input
+        filterEl.parentNode.style.display = 'block';
+      } else if (features.length === 0 && filterEl.value !== '') {
+        empty.textContent = 'No results found';
+        listingEl.appendChild(empty);
+      } else {
+        empty.textContent = 'Drag the map to populate results';
+        listingEl.appendChild(empty);
+
+        // Hide the filter input
+        filterEl.parentNode.style.display = 'none';
+
+        // remove features filter
+        map.setFilter('brand', ['has', 'abbrev']);
+      }
+    }
+
+    function normalize(string) {
+      return string.trim().toLowerCase();
+    }
+
+    // Because features come from tiled vector data,
+    // feature geometries may be split
+    // or duplicated across tile boundaries.
+    // As a result, features may appear
+    // multiple times in query results.
+    function getUniqueFeatures(features, comparatorProperty) {
+      const uniqueIds = new Set();
+      const uniqueFeatures = [];
+      for (const feature of features) {
+        const id = feature.properties[comparatorProperty];
+        if (!uniqueIds.has(id)) {
+          uniqueIds.add(id);
+          uniqueFeatures.push(feature);
+        }
+      }
+      return uniqueFeatures;
+    }
+
+    map.on('load', () => {
+      map.addSource('brands', {
+        'type': 'vector',
+        'url': 'mapbox://mapbox.04w69w5j'
+      });
+      map.addLayer({
+        'id': 'brand',
+        'source': 'brands',
+        'source-layer': 'ne_10m_airports',
+        'type': 'circle',
+        'paint': {
+          'circle-color': '#4264fb',
+          'circle-radius': 4,
+          'circle-stroke-width': 2,
+          'circle-stroke-color': '#ffffff'
+        }
+      });
+
+      map.on('movestart', () => {
+        // reset features filter as the map starts moving
+        map.setFilter('brand', ['has', 'abbrev']);
+      });
+
+      map.on('moveend', () => {
+        const features = map.queryRenderedFeatures({ layers: ['brand'] });
+
+        if (features) {
+          const uniqueFeatures = getUniqueFeatures(features, 'iata_code');
+          // Populate features for the listing overlay.
+          renderListings(uniqueFeatures);
+
+          // Clear the input container
+          filterEl.value = '';
+
+          // Store the current features in sn `brands` variable to
+          // later use for filtering on `keyup`.
+          brands = uniqueFeatures;
+        }
+      });
+
+      map.on('mousemove', 'brand', (e) => {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+
+        // Populate the popup and set its coordinates based on the feature.
+        const feature = e.features[0];
+        popup
+          .setLngLat(feature.geometry.coordinates)
+          .setText(
+            `${feature.properties.name} (${feature.properties.abbrev})`
+          )
+          .addTo(map);
+      });
+
+      map.on('mouseleave', 'brand', () => {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+      });
+
+      filterEl.addEventListener('keyup', (e) => {
+        const value = normalize(e.target.value);
+
+        // Filter visible features that match the input value.
+        const filtered = [];
+        for (const feature of brands) {
+          const name = normalize(feature.properties.name);
+          const code = normalize(feature.properties.abbrev);
+          if (name.includes(value) || code.includes(value)) {
+            filtered.push(feature);
+          }
+        }
+
+        // Populate the sidebar with filtered results
+        renderListings(filtered);
+
+        // Set the filter to populate features into the layer.
+        if (filtered.length) {
+          map.setFilter('brand', [
+            'match',
+            ['get', 'abbrev'],
+            filtered.map((feature) => {
+              return feature.properties.abbrev;
+            }),
+            true,
+            false
+          ]);
+        }
+      });
+
+      // Call this function on initialization
+      // passing an empty array to render an empty state
+      renderListings([]);
+    });
+
 
     ////////////////// OPTIONS ON THE MAP ////////////////////
     const nav = new mapboxgl.NavigationControl()
@@ -40,8 +199,8 @@ const initMapbox = () => {
         marker: true
       })
     ).addControl(nav)
-      .addControl(geolocate)
-      .addControl(full_screen);
+     .addControl(geolocate)
+     .addControl(full_screen);
 
     ////////////////// PARSE EACH DATA TO THE MAP ////////////////////
     const markers = JSON.parse(mapElement.dataset.markers);
@@ -49,6 +208,7 @@ const initMapbox = () => {
 
     markers.forEach((marker) => {
       ////////////////// OPTIONS ON THE MARKER ////////////////////
+
       const popup = new mapboxgl.Popup({
         offset: 25,
         closeOnClick: false
@@ -70,13 +230,12 @@ const initMapbox = () => {
         // popup.remove(map)
       });
 
-      result_marker.getElement().addEventListener('click', (e) => {
+      result_marker.getElement().addEventListener('click', () => {
         map.flyTo({
           center: [marker.lng, marker.lat],
           zoom: 12
         });
       });
-
     });
 
     fitMapToMarkers(map, markers);
